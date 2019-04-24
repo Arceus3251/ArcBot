4765d34e4ee3c739741f6873b3933181d4d83fb9
arceus3251 <arceus3251@gmail.com>
2019-04-24 17:46:37 -0500

    WE HAVE LIFE

---
+++ src/main/java/MyListener.java
@@ -0,0 +1,9 @@
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+public class MyListener extends ListenerAdapter{
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event){
+        System.out.println(event.getMessage().getContentDisplay());
+    }
+}
--- src/main/java/TerminalMain.java
+++ src/main/java/TerminalMain.java
@@ -1,22 +1,19 @@
 import net.dv8tion.jda.api.AccountType;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.MessageChannel;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
+
+import javax.annotation.Nonnull;
 import javax.security.auth.login.LoginException;
 
 public class TerminalMain extends ListenerAdapter {
     public static void main(String[] args) throws LoginException {
         String token = "NTY5ODU5NTM1OTIyMjY2MTQy.XMCB-Q.HSSRhFKhipBPsCa0wjYyeuUoccc";
-        JDA api = new JDABuilder(token).build();
-        api.addEventListener(new Bot());
-    }
-    @Override
-    public void onMessageReceived(@NotNull MessageReceivedEvent event){
-        System.out.println(event.getMessage().getContentDisplay());
-        if(event.getMessage().getContentRaw().equals("Ping")){
-            event.getChannel().sendMessage("Pong").queue();
-        }
+        JDA builder = new JDABuilder(token).build();
+        builder.addEventListener(new MyListener());
     }
 }
